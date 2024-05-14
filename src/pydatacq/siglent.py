67c72e80f849de46ae7465d7077d248cd6f279be
kvdijken <koen.van.dijken@gmail.com>
2024-05-14 23:02:56 +0200

    docstring

--- src/pydatacq/siglent.py
+++ src/pydatacq/siglent.py
@@ -2,21 +2,27 @@ import asyncio
 
 class Siglent():
 
+    # 
     def __init__(self,ip,port):
         self.ip = ip
         self.port = port
 
 
+    # 
     async def async_query(self,cmd,size=8000):
-        '''Perform an asynchronous query. Can be used for
+        '''
+        Perform an asynchronous query. Can be used for
         strings and for binary data, although for binary
         data it may be better to use a special purpose function
         which knows the protocol for the binary data.
         
         Parameters:
+
+        cmd : (str) command to be sent to the device.
         
         Returns:
         
+        Returns the data in the format as defined by the device.
         '''
         reader, writer = await asyncio.open_connection(self.ip,self.port)
 
@@ -25,22 +31,34 @@ class Siglent():
             writer.write(b_cmd + b'\n')
             await writer.drain()
             data = await reader.read(size)
-    #        data = await reader.read()
         finally:
             writer.close()
             await writer.wait_closed()
         return data
 
 
+    # 
     def query(self,cmd,size=8000):
-        '''Perform a synchronous query. Can be used
-        to perform queries which return strings, like *IDN?'''
+        '''
+        Perform a synchronous query. Can be used
+        to perform queries which return strings, like *IDN?
+
+        Parameters:
+
+        cmd : (str) query to be sent to the device
+
+        Returns:
+        
+        Returns the data in the format as defined by the device.
+        '''
         data = asyncio.run(self.async_query(cmd,size))
         return data
     
 
+    # 
     def query_string(self,cmd,size=8000):
-        '''Perform a synchronous query for a string result.
+        '''
+        Perform a synchronous query for a string result.
         
         Parameters:
         
@@ -48,12 +66,19 @@ class Siglent():
         
         Returns:
         
-        The string (str) returned from the device.'''
+        The string (str) returned from the device.
+        '''
         return self.query(cmd).decode('ascii')
 
 
+    # 
     async def async_send(self,cmd):
         '''
+        Asynchronously sends a command to the device.
+
+        Parameters:
+
+        cmd : (str) command to be sent to the device.
         '''
         _, writer = await asyncio.open_connection(self.ip,self.port)
         b_cmd = bytes(cmd,'ascii')
@@ -63,7 +88,13 @@ class Siglent():
         await writer.wait_closed()
 
 
+    # 
     def send(self,cmd):
         '''
+        Synchronously sends a command to the device.
+
+        Parameters:
+
+        cmd : (str) command to be sent to the device.
         '''
         asyncio.run(self.async_send(cmd))
